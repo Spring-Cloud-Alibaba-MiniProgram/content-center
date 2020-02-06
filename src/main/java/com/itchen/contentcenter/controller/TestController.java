7bf18a36319e8537c97bef36f51343e4f9d224d5
BibiChen <yongbing923@gmail.com>
2020-02-06 19:33:50 +0800

    服务发现-Nacos

--- src/main/java/com/itchen/contentcenter/controller/TestController.java
+++ src/main/java/com/itchen/contentcenter/controller/TestController.java
@@ -3,6 +3,8 @@ package com.itchen.contentcenter.controller;
 import com.itchen.contentcenter.dao.share.ShareMapper;
 import com.itchen.contentcenter.domain.entity.share.Share;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,9 @@ public class TestController {
     @Autowired
     private ShareMapper shareMapper;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("test")
     public List<Share> testInsert() {
         // 1. 插入
@@ -36,6 +41,17 @@ public class TestController {
         // 2. 查询
         List<Share> shares = this.shareMapper.selectAll();
         return shares;
+    }
+
+    /**
+     * 测试：服务发现，证明内容中心总能找到用户中心
+     *
+     * @return 用户中心所有实例的地址信息
+     */
+    @GetMapping("/test2")
+    public List<ServiceInstance> getInstances() {
+        // 查询指定服务的所有实例的信息
+        return this.discoveryClient.getInstances("user-center");
     }
 
 }
--- src/main/java/com/itchen/contentcenter/service/content/ShareService.java
+++ src/main/java/com/itchen/contentcenter/service/content/ShareService.java
@@ -4,10 +4,15 @@ import com.itchen.contentcenter.dao.share.ShareMapper;
 import com.itchen.contentcenter.domain.dto.content.ShareDTO;
 import com.itchen.contentcenter.domain.dto.user.UserDTO;
 import com.itchen.contentcenter.domain.entity.share.Share;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * 分享服务 .
@@ -17,6 +22,7 @@ import org.springframework.web.client.RestTemplate;
  * @since 2020-02-06
  */
 @Service
+@Slf4j
 public class ShareService {
 
     @Autowired
@@ -25,14 +31,25 @@ public class ShareService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     public ShareDTO findById(Integer id) {
         // 获取分享详情
         Share share = this.shareMapper.selectByPrimaryKey(id);
         Integer userId = share.getUserId();
         // 怎么调用用户微服务的 /users/{id}
 
+        // 用户中心所有实例的信息
+        List<ServiceInstance> instances = this.discoveryClient.getInstances("user-center");
+        String targetURL = instances.stream()
+                .map(instance -> instance.getUri().toString() + "/users/{id}")
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("当前没有实例！"));
+
+        log.info("请求的目标地址：{}", targetURL);
         UserDTO userDTO = restTemplate.getForObject(
-                "http://localhost:8080/users/{id}",
+                targetURL,
                 UserDTO.class, userId
         );
         ShareDTO shareDTO = new ShareDTO();
