06363eeaadd2e7783f38905233e516baffb74dde
BibiChen <yongbing923@gmail.com>
2020-03-02 22:56:07 +0800

    配置属性动态刷新与回滚

--- src/main/java/com/itchen/contentcenter/test/TestController.java
+++ src/main/java/com/itchen/contentcenter/test/TestController.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -40,6 +41,7 @@ import java.util.List;
  */
 @Slf4j
 @RestController
+@RefreshScope
 public class TestController {
 
     @Autowired
